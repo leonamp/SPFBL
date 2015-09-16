b8b6afcb5f09cd8da0c413886b9c014ab69ab0d7
Roberto Lima <roberto@cronus-emulator.com>
2015-09-15 22:03:51 -0300

    Create spfbl-restart.sh

---
+++ spfbl-restart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin
+
+if [ "$(ps auxwf | grep SPFBL | wc -l)" -eq "1" ]; then
+    echo $(echo "SHUTDOWN" | nc 127.0.0.1 9875)
+    java -jar /opt/spfb/SPFBL.jar 9875 512 >> /var/log/spfbl/activity.log &
+fi
