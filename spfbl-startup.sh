8041a54cf4bcb5d61e37442b90535ac91cea9892
Roberto Lima <roberto@cronus-emulator.com>
2015-09-15 22:01:23 -0300

    Create spfbl-startup.sh

---
+++ spfbl-startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin
+
+if [ "$(ps auxwf | grep SPFBL | wc -l)" -lt "1" ]; then
+    cd /var/lib/spfbl
+    java -jar /opt/spfb/SPFBL.jar 9875 512 >> /var/log/spfbl/activity.log &
+fi
