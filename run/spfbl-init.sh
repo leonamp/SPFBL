e5de01cbce3985b765e93df5e30968c04797d954
Roberto Lima <roberto@cronus-emulator.com>
2015-10-03 11:04:36 -0300

    Update spfbl-init.sh

--- run/spfbl-init.sh
+++ run/spfbl-init.sh
@@ -5,31 +5,31 @@
 ####
 
 start() {
-cd /opt/spfbl/dist/
-/usr/bin/java -jar /opt/spfbl/dist/SPFBL.jar 9875 512 >> /opt/spfbl/log/spfbl.log &
+    cd /opt/spfbl/
+    /usr/bin/java -jar /opt/spfbl/SPFBL.jar 9875 512 >> /var/log/spfbl/activity-$(date "+%Y-%m-%d").log &
 }
 
 stop() {
-echo "SHUTDOWN" | nc 127.0.0.1 9875
+    echo "SHUTDOWN" | nc 127.0.0.1 9875
 }
 
 case "$1" in
-start)
-echo "[start] Iniciando serviço do SPFBL"
-start
-;;
-stop)
-echo "[stop] Parando o serviço do SPFBL"
-stop
-;;
-restart)
-echo "[restart] Reiniciando serviço do SPFBL"
-stop
-start
-;;
-*)
-echo "*** Usage: $0 {start|stop|restart}"
-exit 1
+    start)
+        echo "[start] Iniciando serviço do SPFBL"
+        start
+    ;;
+    stop)
+        echo "[stop] Parando o serviço do SPFBL"
+        stop
+    ;;
+    restart)
+        echo "[restart] Reiniciando serviço do SPFBL"
+        stop
+        start
+    ;;
+    *)
+        echo "*** Usage: $0 {start|stop|restart}"
+        exit 1
 esac
 
 exit 0
