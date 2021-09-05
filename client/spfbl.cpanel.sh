1d1328303045b986a881916ef2b37a023d0d39db
Leandro Carlos Rodrigues <leandro.carlos.rodrigues@gmail.com>
2021-09-05 12:35:24 -0300

    SPFBL Cheker installer for cPanel

---
+++ client/spfbl.cpanel.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+#
+# An SPFBL Cheker installer for cPanel.
+#
+# Usage:
+#    ./spfbl.cpanel.sh [install|uninstall] 
+#    
+# SPFBL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# SPFBL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with SPFBL. If not, see <http://www.gnu.org/licenses/>.
+#
+# Project SPFBL - Copyright Leandro Carlos Rodrigues - leandro@spfbl.net
+# https://github.com/leonamp/SPFBL
+#
+# Version: 1.1
+
+install() {
+    if command -v apt-get >/dev/null; then
+      sudo apt-get install nmap ncat
+    elif command -v yum >/dev/null; then
+      yum install -y nmap nc
+    else
+      exit 1
+    fi
+    cd /usr/local/bin/
+    wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl.sh
+    sudo chmod +x spfbl.sh
+    mv spfbl.sh spfbl
+    cd /usr/local/cpanel/etc/exim/acls/ACL_RECIPIENT_BLOCK/
+    wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/custom_end_recipient_spfbl
+    cd /usr/local/cpanel/etc/exim/acls/ACL_CHECK_MESSAGE_PRE_BLOCK/
+    wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/custom_end_check_message_pre_spfbl
+    service exim restart
+}
+
+uninstall() {
+    rm /usr/local/cpanel/etc/exim/acls/ACL_RECIPIENT_BLOCK/custom_end_recipient_spfbl
+    rm /usr/local/cpanel/etc/exim/acls/ACL_CHECK_MESSAGE_PRE_BLOCK/custom_end_check_message_pre_spfbl
+    service exim restart
+}
+
+case "$1" in
+    install)
+        echo "[install] Installing SPFBL Checker powered by SPFBL.net"
+        install
+    ;;
+    uninstall)
+        echo "[uninstall] Uninstalling SPFBL Checker powered by SPFBL.net"
+        uninstall
+    ;;
+    *)
+        echo "*** Usage: $0 [install|uninstall]"
+        exit 1
+esac
