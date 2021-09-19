36ae8f35b481ae4c9169e405eb04e2ce65c3c74a
Leandro Carlos Rodrigues <leandro.carlos.rodrigues@gmail.com>
2021-09-19 12:38:52 -0300

    Update spfbl.cpanel.sh

--- client/spfbl.cpanel.sh
+++ client/spfbl.cpanel.sh
@@ -48,12 +48,12 @@ install() {
         wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/master.conf -O /etc/clamav-unofficial-sigs/master.conf
         wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/user.conf -O /etc/clamav-unofficial-sigs/user.conf
         DISTRO=$(cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(centos-7|centos-6|ubuntu)' | uniq)
-        if [ $DISTRO eq "centos-7" ]; then
+        if [ $DISTRO == "centos-7" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
             echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
-        elif [ $DISTRO eq "centos-6" ]; then
+        elif [ $DISTRO == "centos-6" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-        elif [ $DISTRO eq "ubuntu" ]; then
+        elif [ $DISTRO == "ubuntu" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.ubuntu.conf" -O /etc/clamav-unofficial-sigs/os.conf
         else
             echo "Linux distro not identified."
