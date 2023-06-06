f8d0f01d9fdaf831f7242cfcc19db570f8775b13
Leandro Carlos Rodrigues <leandro.carlos.rodrigues@gmail.com>
2023-06-06 17:03:51 -0300

    Update spfbl.cpanel.sh

--- client/spfbl.cpanel.sh
+++ client/spfbl.cpanel.sh
@@ -35,124 +35,101 @@ function exim_configuration() {
 }
 
 function install() {
-#    # Install netcat.
-#    if command -v apt-get >/dev/null; then
-#        apt-get install nmap ncat
-#    elif command -v yum >/dev/null; then
-#        yum install -y nmap nc
-#    else
-#        echo "Linux installation tool not identified."
-#        echo "Please contact us to update this installation script to it works for your distro."
-#        echo "https://spfbl.net/en/contact"
-#        exit 1
-#    fi
-#    # Install SPFBL client script.
-#    wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl.sh -O /usr/local/bin/spfbl
-#    chmod +x /usr/local/bin/spfbl
-#    /usr/local/bin/spfbl version
-#    if [ $? -eq 0 ]; then
-#        # Enable Clamav.
-#        /usr/local/cpanel/scripts/update_local_rpm_versions --edit target_settings.clamav installed
-#        /usr/local/cpanel/scripts/check_cpanel_rpms --fix --targets=clamav
-#        
-#        # Install clamav-unofficial-sigs.
-#        mkdir -p /etc/clamav-unofficial-sigs/
-#        wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/master.conf -O /etc/clamav-unofficial-sigs/master.conf
-#        wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/user.conf -O /etc/clamav-unofficial-sigs/user.conf
-#        printf "\ndeclare -a additional_dbs=(\n\thttps://matrix.spfbl.net/spfbl.hsb\n\thttps://matrix.spfbl.net/spfbl.ign2\n\thttps://matrix.spfbl.net/cpanel.ign2\n) #END ADDITIONAL DATABASES\n\n" >> /etc/clamav-unofficial-sigs/user.conf
-#        DISTRO=$(cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(centos-8|centos-7|centos-6|ubuntu|cloudlinux 7|cloudlinux 8|almalinux-8|cloudlinux server release 6|centos release 6)' | uniq)
-#        if [ "$DISTRO" = "centos-7" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "centos-8" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "centos-6" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "cloudlinux server release 6" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "centos release 6" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "ubuntu" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.ubuntu.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "cloudlinux 7" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "cloudlinux 8" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
-#        elif [ "$DISTRO" = "almalinux-8" ]; then
-#            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
-#            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
-#        else
-#            echo "Linux distro not identified."
-#            echo "Please contact us to update this installation script to it works for your distro."
-#            echo "https://spfbl.net/en/contact"
-#            exit 1;
-#        fi
-#        wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/clamav-unofficial-sigs.sh -O /usr/local/bin/clamav-unofficial-sigs.sh
-#        chmod 755 /usr/local/bin/clamav-unofficial-sigs.sh
-#        /usr/local/bin/clamav-unofficial-sigs.sh --force
-#        /usr/local/bin/clamav-unofficial-sigs.sh --install-cron --install-logrotate
-#        
-#        # Install SPFBL configuration files.
-#        wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl_end_recipient -O /usr/local/cpanel/etc/exim/acls/ACL_RECIPIENT_BLOCK/spfbl_end_recipient
-#        wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl_begin_smtp_dkim -O /usr/local/cpanel/etc/exim/acls/ACL_SMTP_DKIM_BLOCK/spfbl_begin_smtp_dkim
-#        wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl_begin_check_message_pre -O /usr/local/cpanel/etc/exim/acls/ACL_CHECK_MESSAGE_PRE_BLOCK/spfbl_begin_check_message_pre
-#        
-#        # Config Exim Configuration Manager interface.
-#        exim_configuration "acl_delay_unknown_hosts" "0"
-#        exim_configuration "acl_dkim_disable" "0"
-#        exim_configuration "acl_dkim_bl" "0"
-#        exim_configuration "acl_spam_scan_secondarymx" "0"
-#        exim_configuration "acl_outgoing_spam_scan" "0"
-#        exim_configuration "acl_default_exiscan" "0"
-#        exim_configuration "acl_default_spam_scan" "0"
-#        exim_configuration "acl_default_spam_scan_check" "0"
-##        sed '/@CONFIG@/a timeout_frozen_after = 7d' /etc/exim.conf.local > spfbltemp && mv -f spfbltemp /etc/exim.conf.local
-##        sed '/@CONFIG@/a spamd_address = 54.233.253.229 9877 retry=30s tmo=3m' /etc/exim.conf.local > spfbltemp && mv -f spfbltemp /etc/exim.conf.local
-##        sed '/@CONFIG@/a smtp_accept_max = 250' /etc/exim.conf.local > spfbltemp && mv -f spfbltemp /etc/exim.conf.local
-#        
-#        # Restart cPanel service.
-#        /usr/local/cpanel/scripts/buildeximconf
-#        /usr/local/cpanel/scripts/restartsrv_exim
-#        
-#        echo "SPFBL Checker was successfully installed!"
-#        echo ""
-
-
-        read -p "Você deseja continuar? (yes/no): " resposta
-
-
-        # Verifica a resposta
-        if [ "$resposta" = "yes" ]; then
-           echo "Você escolheu 'yes'."
-        elif [ "$resposta" = "no" ]; then
-           echo "Você escolheu 'no'."
+    # Install netcat.
+    if command -v apt-get >/dev/null; then
+        apt-get install nmap ncat
+    elif command -v yum >/dev/null; then
+        yum install -y nmap nc
+    else
+        echo "Linux installation tool not identified."
+        echo "Please contact us to update this installation script to it works for your distro."
+        echo "https://spfbl.net/en/contact"
+        exit 1
+    fi
+    # Install SPFBL client script.
+    wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl.sh -O /usr/local/bin/spfbl
+    chmod +x /usr/local/bin/spfbl
+    /usr/local/bin/spfbl version
+    if [ $? -eq 0 ]; then
+        # Enable Clamav.
+        /usr/local/cpanel/scripts/update_local_rpm_versions --edit target_settings.clamav installed
+        /usr/local/cpanel/scripts/check_cpanel_rpms --fix --targets=clamav
+        
+        # Install clamav-unofficial-sigs.
+        mkdir -p /etc/clamav-unofficial-sigs/
+        wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/master.conf -O /etc/clamav-unofficial-sigs/master.conf
+        wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/user.conf -O /etc/clamav-unofficial-sigs/user.conf
+        printf "\ndeclare -a additional_dbs=(\n\thttps://matrix.spfbl.net/spfbl.hsb\n\thttps://matrix.spfbl.net/spfbl.ign2\n\thttps://matrix.spfbl.net/cpanel.ign2\n) #END ADDITIONAL DATABASES\n\n" >> /etc/clamav-unofficial-sigs/user.conf
+        DISTRO=$(cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(centos-8|centos-7|centos-6|ubuntu|cloudlinux 7|cloudlinux 8|almalinux-8|cloudlinux server release 6|centos release 6)' | uniq)
+        if [ "$DISTRO" = "centos-7" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "centos-8" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "centos-6" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "cloudlinux server release 6" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "centos release 6" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "ubuntu" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.ubuntu.conf" -O /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "cloudlinux 7" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "cloudlinux 8" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "almalinux-8" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
+            echo 'clamscan_bin="/usr/local/cpanel/3rdparty/bin/clamscan"' >> /etc/clamav-unofficial-sigs/os.conf
         else
-           echo "Resposta inválida."
+            echo "Linux distro not identified."
+            echo "Please contact us to update this installation script to it works for your distro."
+            echo "https://spfbl.net/en/contact"
+            exit 1;
         fi
-	
-#	read -p "Do you want to install the SPFBL Firewall solution too? (Yes/No) " resposta
-#	case $resposta in
-#	    [Yy]* )
-#	        echo "Installing SPFBL Firewall solution..."
-#                firewall
-#	    ;;
-#	    * )
-#	        echo "SPFBL Firewall solution was not installed."
-#	    ;;
-#	esac
-	
-#    else
-#        myIP=$(curl -s http://checkip.amazonaws.com/)
-#        myHOST=$(hostname)
-#        echo "Your cPanel doesn't have permission to access matrix.spfbl.net server yet."
-#        echo "Please contact us to get your permission for the host $myHOST [$myIP]."
-#        echo "https://spfbl.net/en/contact"
-#        echo "If this host has already it, open the port 9877 TCP OUT in your firewall."
-#        exit 1;
-#    fi
+        wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/clamav-unofficial-sigs.sh -O /usr/local/bin/clamav-unofficial-sigs.sh
+        chmod 755 /usr/local/bin/clamav-unofficial-sigs.sh
+        /usr/local/bin/clamav-unofficial-sigs.sh --force
+        /usr/local/bin/clamav-unofficial-sigs.sh --install-cron --install-logrotate
+        
+        # Install SPFBL configuration files.
+        wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl_end_recipient -O /usr/local/cpanel/etc/exim/acls/ACL_RECIPIENT_BLOCK/spfbl_end_recipient
+        wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl_begin_smtp_dkim -O /usr/local/cpanel/etc/exim/acls/ACL_SMTP_DKIM_BLOCK/spfbl_begin_smtp_dkim
+        wget https://raw.githubusercontent.com/leonamp/SPFBL/master/client/spfbl_begin_check_message_pre -O /usr/local/cpanel/etc/exim/acls/ACL_CHECK_MESSAGE_PRE_BLOCK/spfbl_begin_check_message_pre
+        
+        # Config Exim Configuration Manager interface.
+        exim_configuration "acl_delay_unknown_hosts" "0"
+        exim_configuration "acl_dkim_disable" "0"
+        exim_configuration "acl_dkim_bl" "0"
+        exim_configuration "acl_spam_scan_secondarymx" "0"
+        exim_configuration "acl_outgoing_spam_scan" "0"
+        exim_configuration "acl_default_exiscan" "0"
+        exim_configuration "acl_default_spam_scan" "0"
+        exim_configuration "acl_default_spam_scan_check" "0"
+#        sed '/@CONFIG@/a timeout_frozen_after = 7d' /etc/exim.conf.local > spfbltemp && mv -f spfbltemp /etc/exim.conf.local
+#        sed '/@CONFIG@/a spamd_address = 54.233.253.229 9877 retry=30s tmo=3m' /etc/exim.conf.local > spfbltemp && mv -f spfbltemp /etc/exim.conf.local
+#        sed '/@CONFIG@/a smtp_accept_max = 250' /etc/exim.conf.local > spfbltemp && mv -f spfbltemp /etc/exim.conf.local
+        
+        # Restart cPanel service.
+        /usr/local/cpanel/scripts/buildeximconf
+        /usr/local/cpanel/scripts/restartsrv_exim
+        
+        echo "SPFBL Checker was successfully installed!"
+        echo ""
+        echo "Installing SPFBL Firewall solution..."
+	firewall
+    else
+        myIP=$(curl -s http://checkip.amazonaws.com/)
+        myHOST=$(hostname)
+        echo "Your cPanel doesn't have permission to access matrix.spfbl.net server yet."
+        echo "Please contact us to get your permission for the host $myHOST [$myIP]."
+        echo "https://spfbl.net/en/contact"
+        echo "If this host has already it, open the port 9877 TCP OUT in your firewall."
+        exit 1;
+    fi
 }
 
 function update() {
