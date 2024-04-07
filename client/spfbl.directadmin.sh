e6db7095f70f7c3f3d3e2a282bcc48171d64ff91
Leandro Carlos Rodrigues <leandro.carlos.rodrigues@gmail.com>
2024-04-07 09:50:02 -0300

    Update spfbl.directadmin.sh

--- client/spfbl.directadmin.sh
+++ client/spfbl.directadmin.sh
@@ -65,7 +65,7 @@ function install() {
         sed -i 's/^#clamd_restart_opt="service clamd restart"/clamd_restart_opt="service clamd@scan restart"/' /etc/clamav-unofficial-sigs/master.conf
         wget https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/user.conf -O /etc/clamav-unofficial-sigs/user.conf
         printf "\ndeclare -a additional_dbs=(\n\thttps://matrix.spfbl.net/spfbl.hsb\n\thttps://matrix.spfbl.net/spfbl.ign2\n) #END ADDITIONAL DATABASES\n\n" >> /etc/clamav-unofficial-sigs/user.conf
-        DISTRO=$(cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(centos-8|centos stream 8|centos-7|centos-6|ubuntu|cloudlinux 7|cloudlinux 8|almalinux-8|almalinux-9|cloudlinux server release 6|cloudLinux release 9|centos release 6|debian gnu/linux 7|debian gnu/linux 8|debian gnu/linux 10|debian gnu/linux 11)' | sort | uniq)
+        DISTRO=$(cat /etc/*-release | tr [:upper:] [:lower:] | grep -Poi '(centos-8|centos stream 8|centos-7|centos-6|ubuntu|cloudlinux 7|cloudlinux 8|cloudlinux 9|almalinux-8|almalinux-9|cloudlinux server release 6|centos release 6|debian gnu/linux 7|debian gnu/linux 8|debian gnu/linux 10|debian gnu/linux 11)' | sort | uniq)
         if [ "$DISTRO" = "centos-7" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos7-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
         elif [ "$DISTRO" = "centos-8" ]; then
@@ -76,8 +76,6 @@ function install() {
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6.conf" -O /etc/clamav-unofficial-sigs/os.conf
         elif [ "$DISTRO" = "cloudlinux server release 6" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6.conf" -O /etc/clamav-unofficial-sigs/os.conf
-        elif [ "$DISTRO" = "cloudLinux release 9" ]; then
-            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6-cpanel.conf" -O /etc/clamav-unofficial-sigs/os.conf
         elif [ "$DISTRO" = "centos release 6" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6.conf" -O /etc/clamav-unofficial-sigs/os.conf
         elif [ "$DISTRO" = "ubuntu" ]; then
@@ -93,6 +91,8 @@ function install() {
         elif [ "$DISTRO" = "cloudlinux 7" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6.conf" -O /etc/clamav-unofficial-sigs/os.conf
         elif [ "$DISTRO" = "cloudlinux 8" ]; then
+            wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6.conf" -O /etc/clamav-unofficial-sigs/os.conf
+        elif [ "$DISTRO" = "cloudlinux 9" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6.conf" -O /etc/clamav-unofficial-sigs/os.conf
         elif [ "$DISTRO" = "almalinux-8" ]; then
             wget "https://raw.githubusercontent.com/extremeshok/clamav-unofficial-sigs/master/config/os/os.centos6.conf" -O /etc/clamav-unofficial-sigs/os.conf
